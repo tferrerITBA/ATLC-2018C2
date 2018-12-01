b46335e770937603906f3437384104289075e2fc
tferrerITBA <tferrer@itba.edu.ar>
2018-12-01 20:26:47 -0300

    Allows negative double literals and updates test 3

--- test3.f
+++ test3.f
@@ -1,21 +1,23 @@
 
 frozone() {
 	x = 0
+	first = true
 	y = 0
 	z = 0
-	print("Please enter a number. You will then have to enter that amount of numbers.")
+	print("Please enter a number greater than 0. You will then have to enter that amount of numbers.")
 	scan(x, 1)
-	on (x lt 0) do {
-		print("Number must be greater than 0.")
+	on (x lt 1) do {
+		print("Number must be greater than 1.")
 	} else {
 		cycle {
 			scan(y, 1.0)
-			on (y gt z) do {
+			on ((y gt z) or (first eq true)) do {
 				z = y + 0
 			}
 			x = x - 1
+			first = false
 		} on (x gt 0)
+		print("The largest number entered is: ")
+		print(z)
 	}
-	print("The largest number entered is: ")
-	print(z)
 }
